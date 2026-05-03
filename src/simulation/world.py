4c861f85ca9b501f0fe0f826b40139acc6eb87b7
maff1337 <yakdm@duck.com>
2026-05-04 00:37:00 +0300

    feat: add World

---
+++ src/simulation/world.py
@@ -0,0 +1,63 @@
+from dataclasses import dataclass
+from random import randrange
+from typing import Optional, Sequence
+
+from simulation.coordinates import Coordinates
+from simulation.entities import Entity
+
+
+class World:
+    def __init__(self, rows: int, columns: int) -> None:
+        self._rows = rows
+        self._columns = columns
+
+        self._entities: dict[Entity, Coordinates] = {}
+
+        if self.size() < 5:
+            raise
+
+    @property
+    def rows(self) -> int:
+        return self._rows
+
+    @property
+    def columns(self) -> int:
+        return self._columns
+
+    def size(self) -> int:
+        return self._rows * self._columns
+
+    def get_entities(self) -> Sequence[Entity]:
+        return list(self._entities.keys())
+
+    def get_all_coordinates(self) -> Sequence[Coordinates]:
+        return list(self._entities.values())
+
+    def _get_random_coordinates(self) -> Coordinates:
+        row = randrange(self._rows)
+        column = randrange(self._columns)
+
+        return Coordinates(row, column)
+
+    def get_empty_coordinates(self) -> Coordinates:
+        while True:
+            coords = self._get_random_coordinates()
+
+            if coords not in self._entities.values():
+                return coords
+
+    def add_entity(self, entity: Entity) -> None:
+        coords = self.get_empty_coordinates()
+
+        self._entities[entity] = coords
+
+    def delete_entity(self, entity: Entity) -> None:
+        if entity in self._entities.keys():
+            del self._entities[entity]
+
+    def get_entity(self, coordinates: Coordinates) -> Optional[Entity]:
+        for entity, coords in self._entities.items():
+            if coords == coordinates:
+                return entity
+
+        return None
